80fb6c73fd59af520470b0c4728711eb56a0756e
久松勇斗 <>
2019-10-27 17:25:19 +0900

    topに戻るボタンを更新

--- app.js
+++ app.js
@@ -176,6 +176,24 @@ $(function(){
     };
 
 
+    // トップに戻るボタン
+    $('#back-to-top').hide();
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 1500) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+
+    $('#back-to-top a').click(function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
 
 
 
